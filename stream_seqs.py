d7f0f05e73427dc5359ed77b766da098e267c6dc
Stephen Nayfach <snayfach@denovo1.nersc.gov>
2018-07-18 07:42:34 -0700

    First

---
+++ stream_seqs.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+import sys, argparse, utility
+
+def readfq(fp):
+	""" https://github.com/lh3/readfq/blob/master/readfq.py
+		A generator function for parsing fasta/fastq records """
+	last = None # this is a buffer keeping the last unprocessed line
+	while True: # mimic closure; is it a bad idea?
+		if not last: # the first record or a record following a fastq
+			for l in fp: # search for the start of the next record
+				if l[0] in '>@': # fasta/q header line
+					last = l[:-1] # save this line
+					break
+		if not last: break
+		name, seqs, last = last[1:].partition(" ")[0], [], None
+		for l in fp: # read the sequence
+			if l[0] in '@+>':
+				last = l[:-1]
+				break
+			seqs.append(l[:-1])
+		if not last or last[0] != '+': # this is a fasta record
+			yield name, ''.join(seqs), None # yield a fasta record
+			if not last: break
+		else: # this is a fastq record
+			seq, leng, seqs = ''.join(seqs), 0, []
+			for l in fp: # read the quality
+				seqs.append(l[:-1])
+				leng += len(l) - 1
+				if leng >= len(seq): # have read enough quality
+					last = None
+					yield name, seq, ''.join(seqs); # yield a fastq record
+					break
+			if last: # reach EOF before reading enough quality
+				yield name, seq, None # yield a fasta record instead
+				break
+
+def main():
+	args = parse_args()
+	reads = 0
+	bp = 0
+	for inpath in args['input']:
+		infile = utility.iopen(inpath)
+		for name, seq, qual in readfq(infile):
+			id = name.split()[0]
+			seq_len = len(seq)
+			if args['read_length']: # trim/filter reads
+				if seq_len < args['read_length']:
+					continue
+				else:
+					seq = seq[0:args['read_length']]
+					seq_len = len(seq)
+			sys.stdout.write('>%s_%s\n%s\n' % (id, seq_len, seq))
+			reads += 1
+			if reads == args['max_reads']:
+				return
+
+def parse_args():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-1', type=str, dest='m1')
+	parser.add_argument('-2', type=str, dest='m2')
+	parser.add_argument('-l', type=int, dest='read_length')
+	parser.add_argument('-n', type=int, dest='max_reads', default=float('Inf'))
+	args = vars(parser.parse_args())
+	args['input'] = []
+	args['input'] += (args['m1'].split(','))
+	if args['m2']: args['input'] += args['m2'].split(',')
+	return args
+
+if __name__ == "__main__":
+	main()
